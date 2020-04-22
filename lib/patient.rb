9901f00d4b08d1dd660c51825c3bef9462044d89
Victor Plushko <vicplushko33@gmail.com>
2020-04-22 04:47:43 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,26 @@
+class Patient
+  attr_accessor :name, :appointment, :doctor
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select {|appt| appt.patient == self}
+  end
+
+  def doctors
+    Appointments.all.collect {|appt| appt.doctor}
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
+  end
+end
